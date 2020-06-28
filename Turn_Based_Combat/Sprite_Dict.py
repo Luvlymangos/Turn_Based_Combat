d0492ff7f6a90fcc642e2345cea0f6a109dff35e
Luvlymangos <46830118+Luvlymangos@users.noreply.github.com>
2020-06-27 21:00:39 -0600

    Tiles

---
+++ Turn_Based_Combat/Sprite_Dict.py
@@ -0,0 +1,64 @@
+road_1 = {
+'name': '1',
+'Collidable': False
+}
+road_2 = {
+'name': '2',
+'Collidable': False
+}
+road_3 = {
+'name': '3',
+'Collidable': False
+}
+road_4 = {
+'name': '4',
+'Collidable': False
+}
+road_5 = {
+'name': '5',
+'Collidable': False
+}
+road_6 = {
+'name': '6',
+'Collidable': False
+}
+road_7 = {
+'name': '7',
+'Collidable': False
+}
+road_8 = {
+'name': '8',
+'Collidable': False
+}
+road_9 = {
+'name': '9',
+'Collidable': False
+}
+road_10 = {
+'name': '10',
+'Collidable': False
+}
+road_11 = {
+'name': '11',
+'Collidable': False
+}
+road_12 = {
+'name': '12',
+'Collidable': False
+}
+road_13 = {
+'name': '13',
+'Collidable': False
+
+
+
+
+Sprite_Dict = {
+'1': road_1,
+'2': road_2,
+'3': road_3,
+'4': road_4,
+'5': road_5,
+'6': road_6,
+'7': road_7
+}
